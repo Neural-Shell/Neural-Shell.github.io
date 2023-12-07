2c151e4977d075a13bd820d000414209cfd06500
Ayush Thakur <111521011+ayush-thakur02@users.noreply.github.com>
2023-12-07 11:53:24 +0530

    fix bug

---
+++ assets/about.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const brandParagraph = document.querySelector('p.brand');
+
+    brandParagraph.addEventListener('click', function (event) {
+        event.preventDefault(); // Prevent the default action of the click event
+        window.open('/', '_self'); // Open the URL "/" in a new tab
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const brandParagraph = document.querySelector('img.logo-logo');
+
+    brandParagraph.addEventListener('click', function (event) {
+        event.preventDefault(); // Prevent the default action of the click event
+        window.open('/', '_self'); // Open the URL "/" in a new tab
+    });
+});
+
+var i = 0;
+var txt = 'Where AI and Development Converge!';
+var speed = 100;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    typeWriter();
+});
